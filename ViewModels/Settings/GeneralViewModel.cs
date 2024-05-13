789f6ad96ec8e81b5eaa28e77732ae2e33286c19
qeezzo <zzzrenatus@gmail.com>
2024-05-13 03:48:11 +0300

    add saving settings

--- ViewModels/Settings/GeneralViewModel.cs
+++ ViewModels/Settings/GeneralViewModel.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
 using translator.Models;
 
 namespace translator.ViewModels.Settings;
 
+[DataContract]
 public class GeneralViewModel : ViewModelBase {
   public struct LanguageItem {
     public string String { get; set; }
@@ -11,6 +12,7 @@ public class GeneralViewModel : ViewModelBase {
   }
 
   public List<LanguageItem> Languages { get; private set; }
+  [DataMember]
   private LanguageItem _Language = new() { String = "en-US", Lang = Lang.English };
   public LanguageItem Language {
     get => _Language;
@@ -21,13 +23,14 @@ public class GeneralViewModel : ViewModelBase {
   }
 
   public List<Engine> Engines { get; private set; }
+  [DataMember]
   private Models.Engine _Engine = Engine.Google;
   public Models.Engine Engine {
     get => _Engine;
     set { _Engine = value; }
   }
 
-  private MainWindowViewModel? MainWindowViewModel;
+  private MainWindowViewModel? MainWindowViewModel = null;
   public GeneralViewModel(MainWindowViewModel m) {
     MainWindowViewModel = m;
 
@@ -36,5 +39,13 @@ public class GeneralViewModel : ViewModelBase {
       new() { String = "ru-RU", Lang = Lang.Russian },
       new() { String = "en-US", Lang = Lang.English }
     };
+  }
+
+  public void Save(string file) {
+    SettingsSerializer.Write(this, file);
+  }
+
+  public static GeneralViewModel? Load(string file) {
+    return SettingsSerializer.Read<GeneralViewModel>(file);
   }
 }
